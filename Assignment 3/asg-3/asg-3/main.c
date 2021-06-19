7892f9164f70320a7dd11b71d0130905ce3f0118
Binit Kumar <kumar.binit1992@gmail.com>
2021-06-18 23:28:58 -0400

    question 2

--- Assignment 3/asg-3/asg-3/main.c
+++ Assignment 3/asg-3/asg-3/main.c
@@ -24,25 +24,40 @@ const element NIL = { .type = LIST, .l = NULL };
 /* 1. element aasel(atom a); AKA atom as element, returns an element whose content
 	is set to atom a.
 */
-element aasel(atom ch) {	
-		
+element aasel(atom a) {	
 	element val;
 	val.type = ATOM;
 	val.l = NULL;
-	val.a = ch;
-	//printf(" val : %c\n", val.a);
+	val.a = a;
+	printf("aasel::val : %c\n", val.a);
 	return val;
 }
 
+/* 2. element lasel(list l); AKA list as element, returns an element whose content is
+set to the list, pointed by l .
+*/
+element lasel(list l) {
+	element val;
+	val.type = ATOM;
+	val.l = l;
+	printf("lasel:: val : %c\n", val.a);
+	return val;	
+}
 
 void main() {
 	printf("Assignment 3\n");
 
 	// 1. aasel
 	atom a = 'A';
-	element x = aasel(a);
-	printf(" atom: %c\n", x.a);
+	element x1 = aasel(a);
+	printf(" atom: %c\n", x1.a);
 	
 	// 2. lasel
+	list l1; 
+	l1 = (list)malloc(sizeof(struct _listnode)); 
+	l1->el = x1;
+	l1->next = NULL;
+	element x2 = lasel(l1);
+	printf("element's atom %c\n", l1->el.a);	
 }
 
